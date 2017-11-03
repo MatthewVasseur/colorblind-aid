f36fa8a25f8d868dc819bc6a0d5b64cfd53bbc95
Matthew Vasseur <mattvasseur@gmail.com>
2017-11-03 14:27:45 -0400

    new code for color naming

--- Colorblind Aid/Utilities.swift
+++ Colorblind Aid/Utilities.swift
@@ -43,5 +43,105 @@ extension UIColor {
         
         self.init(red: newRed, green: newGreen, blue: newBlue, alpha: alpha)
     }
+    
+    /// Return a tuple of RBGa cgfloats (Red, Green, Blue, Alpha)
+    func getRGBa() -> (CGFloat, CGFloat, CGFloat, CGFloat) {
+        var red:   CGFloat = 0
+        var green: CGFloat = 0
+        var blue:  CGFloat = 0
+        var alpha: CGFloat = 0
+        
+        self.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        
+        return (red, green, blue, alpha)
+    }
+    
+    /** Return a tuple of HSL cgfloats (Hue, Saturation, Luminosity)
+     * Adopted from from: Farbtastic 1.2 (http://acko.net/dev/farbtastic)
+     */
+    func getHSL() -> (CGFloat, CGFloat, CGFloat) {
+        let (red, green, blue, _) = self.getRGBa()
+            
+        let minimum = min(red, min(green, blue))
+        let maximum = max(red, max(green, blue))
+        let delta = maximum - minimum
+        let lum = (minimum + maximum) / 2
+        
+        var sat = CGFloat(0)
+        if (lum > 0 && lum < 1) {
+            sat = delta / (lum < 0.5 ? (2.0 * lum) : (2.0 - 2.0 * lum))
+        }
+        
+        var hue = CGFloat(0)
+        if (delta > 0) {
+            if (maximum == red && maximum != green) {
+                hue += (green - blue) / delta
+            }
+            if (maximum == green && maximum != blue) {
+                hue += (2 + (blue - red) / delta)
+            }
+            if (maximum == blue && maximum != red) {
+                hue += (4 + (red - green) / delta)
+            }
+            hue /= 6
+        }
+        print ((hue * 255, sat * 255, lum * 255))
+        return (hue * 255, sat * 255, lum * 255)
+    }
+    
+    /// Get the name of the hue corresponding to the color
+    func toHueName() -> String {
+        let (red, green, blue, _) = self.getRGBa()
+        let (hue, sat, lum) = self.getHSL()
+        
+        var ndf1: CGFloat = 0
+        var ndf2: CGFloat = 0
+        var ndf: CGFloat = 0
+        var df: CGFloat = -1
+        var cl: Int = -1
+        
+        
+        for (i, color) in Constants.colorNames.enumerated() {
+            
+            if (red, green, blue) == (color.red, color.green, color.blue) {
+                return color.hueName
+            }
+            
+            ndf1 = pow(red - color.red, 2) + pow(green - color.green, 2) + pow(blue - color.blue, 2)
+            ndf2 = abs(pow(hue - color.hue, 2)) + pow(sat - color.sat, 2) + abs(pow(lum - color.lum, 2))
+            ndf = ndf1 + ndf2 * 2
+            if (df < 0 || df > ndf) {
+                df = ndf
+                cl = i
+            }
+        }
+        
+        return cl < 0 ? "N/A" : Constants.colorNames[cl].hueName
+    }
 }
+
+//
+//+-----------------------------------------------------------------+
+//|   Created by Chirag Mehta - http://chir.ag/tech/download/ntc    |
+//|-----------------------------------------------------------------|
+//|               ntc js (Name that Color JavaScript)               |
+//+-----------------------------------------------------------------+
+//
+//All the functions, code, lists etc. have been written specifically
+//for the Name that Color JavaScript by Chirag Mehta unless otherwise
+//specified.
+
+//
+//    shades: [
+//    ["FF0000", "Red"],
+//    ["FFA500", "Orange"],
+//    ["FFFF00", "Yellow"],
+//    ["008000", "Green"],
+//    ["0000FF", "Blue"],
+//    ["EE82EE", "Violet"],
+//    ["A52A2A", "Brown"],
+//    ["000000", "Black"],
+//    ["808080", "Grey"],
+//    ["FFFFFF", "White"]
+//    ]
 
